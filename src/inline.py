09b6b42b7c881d13e0dc43a224795501dd0d8bfc
huangmatty <145535332+huangmatty@users.noreply.github.com>
2025-12-24 21:11:30 +0800

    [CH3L4][Extract Links]: Add method to extract image alt text and url, and link anchor text and url.

--- src/inline.py
+++ src/inline.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -27,3 +28,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(nodes)
 
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
