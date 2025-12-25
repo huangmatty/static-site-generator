0b5c282db1dc200fa6c574c2e67b45938bea549e
huangmatty <145535332+huangmatty@users.noreply.github.com>
2025-12-25 13:09:44 +0800

    [CH3L5][Split Images and Links]: Add method to convert TextNode of TEXT type to IMAGE/LINK type.

--- src/inline.py
+++ src/inline.py
@@ -29,6 +29,87 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    # print("begin splitting...")
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue;
+        
+        text = node.text
+        # print(f"original text: {text}\n")
+        matches = extract_markdown_images(text)
+        # print(f"matches: {matches}\n")
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue;
+        
+        nodes = []
+        for match in matches:
+            image_alt = match[0]
+            image_url = match[1]
+            sections = text.split(f"![{image_alt}]({image_url})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown")
+            if len(sections[0]) != 0:
+                text_node = TextNode(sections[0], TextType.TEXT)
+                # print(f"text node: {text_node}\n")
+                nodes.append(text_node)
+            image_node = TextNode(image_alt, TextType.IMAGE, image_url)
+            # print(f"image node: {image_node}\n")
+            nodes.append(image_node)
+            text = sections[1]
+            # print(f"new text: {text}\n")
+        if len(text) > 0:
+            text_node = TextNode(text, TextType.TEXT)
+            # print(f"text node: {text_node}\n")
+            nodes.append(text_node)
+        new_nodes.extend(nodes)
+    # print(f"new nodes: {new_nodes}")
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    print("begin splitting...")
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue;
+        
+        text = node.text
+        print(f"original text: {text}\n")
+        matches = extract_markdown_links(text)
+        print(f"matches: {matches}\n")
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue;
+
+        nodes = []
+        for match in matches:
+            link_anchor = match[0]
+            link_url = match[1]
+            sections = text.split(f"[{link_anchor}]({link_url})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown")
+            if len(sections[0]) != 0:
+                text_node = TextNode(sections[0], TextType.TEXT)
+                print(f"text node: {text_node}\n")
+                nodes.append(text_node)
+            link_node = TextNode(link_anchor, TextType.LINK, link_url)
+            print(f"link node: {link_node}\n")
+            nodes.append(link_node)
+            if len(sections) > 1:
+                text = sections[1]
+                print(f"new text: {text}\n")
+        if len(text) > 0:
+            text_node = TextNode(text, TextType.TEXT)
+            print(f"text node: {text_node}\n")
+            nodes.append(text_node)
+        new_nodes.extend(nodes)
+    print(f"new nodes: {new_nodes}")
+    return new_nodes
+
 def extract_markdown_images(text):
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextType, TextNode
+from inline import split_nodes_image, split_nodes_link
 
 def main():
-    text_node = TextNode("Anchor text for link", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) asdfa asdf fasdk fsk [to boot dev](https://www.boot.dev)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
+    # print(new_nodes)
     return
 
 main()
